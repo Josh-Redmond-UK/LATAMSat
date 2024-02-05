2d309fce21ba6f87ca0cc0f014a3ada626c65af7
Josh Redmond <jr725@exeter.ac.uk>
2024-02-05 20:52:13 +0000

    added iteration for whole roi

--- data/download_data.py
+++ data/download_data.py
@@ -25,17 +25,27 @@ roi = ecoregions.filterBounds(latin_america)
 reduced_res = reduced_res.clip(roi)
 print("got roi")
 
+print("converting to list")
+num_regions = roi.size().getInfo()
+roi_list = ee.List(roi.toList(num_regions))
+
 def sample_landcover(feat):
     sample_roi = reduced_res.clip(feat.geometry())
     sample_geom = sample_roi.stratifiedSample(10, seed=1, geometries=True)
     return sample_geom
 print("generating samples...")
 #reqs = roi.map(sample_landcover).flatten()
-reqs=sample_landcover(roi.first()) # for testing
 print("done")
 print("getting geom")
-reqs = reqs.getInfo()
+reqs_list = []
+for i in range(num_regions):
+    reqs_list.append(sample_landcover(ee.Feature(roi_list.get(i))).aggregate_array('.geo').getInfo())
+    print(f"done with {i+1} of {num_regions}")
+
+#reqs=sample_landcover(roi.first()) # for testing
+
+#reqs = reqs.getInfo()
 print("done, saving...")
-pickle.dump(reqs, open('roi_samples.pkl', 'wb'))
+pickle.dump(reqs_list, open('roi_samples_list.pkl', 'wb'))
 print("done!")
-print(reqs)
+#print(reqs)
