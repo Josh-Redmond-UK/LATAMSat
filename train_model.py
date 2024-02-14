09b4a817b90039d746c68bf29b3b28ee3002f38e
Josh Redmond <jr725@exeter.ac.uk>
2024-02-14 15:36:49 +0000

    removed superfluous print statement

--- train_model.py
+++ train_model.py
@@ -12,7 +12,7 @@ mode = "rgb"
 if mode == "rgb":
     image_shape=(64,64,3)
     model_path = f"models/rgb_model/{datetime.datetime.now().strftime('%Y%m%d-%H%M')}"
-    dataset_glob_pattern = 'latamSatData/datasetRGB_rescaled/**/**/*.png'
+    dataset_glob_pattern = 'latamSatData/datasetRGB_relabel/**/**/*.png'
 
 else:
     image_shape = (64,64,12)
@@ -42,10 +42,10 @@ model.compile(
 train, test = get_dataset(dataset_glob_pattern, mode=mode, shuffle=True)
 
 train_batch_ = next(iter(train))
-print(train_batch_)
+#print(train_batch_)
 show_batch(train_batch_)
 
-history = model.fit(train, validation_data=test, epochs=2, verbose=1, callbacks=[model_callback])
+history = model.fit(train, validation_data=test, epochs=50, verbose=1, callbacks=[model_callback])
 
 plot_training(history, model_path+'/')
 
